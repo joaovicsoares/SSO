f39db2b026c91349dd7c84f5dda07e746ad25bb7
joaovicsoares <victorjoaosoa@gmail.com>
2026-03-02 15:29:44 -0300

    feat: Logs com as tentativas de login

--- Sso.Application/Authentication/AuthenticationService.cs
+++ Sso.Application/Authentication/AuthenticationService.cs
@@ -2,15 +2,17 @@ using Sso.Domain.Repositories;
 using Sso.Domain.Services;
 using Sso.Application.Persistence;
 using Sso.Application.DTOs;
+using Sso.Domain.Entities;
+using Sso.Domain.Enums;
 using Sso.Domain.ValueObjects;
 
 namespace Sso.Application.Authentication;
 
 public class AuthenticationService(
     IUserRepository userRepository,
-    IPasswordHasher passwordHasher
-    // IAuditLogRepository auditLogRepository,
-    // IUnitOfWork unitOfWork
+    IPasswordHasher passwordHasher,
+    IAuditLogRepository auditLogRepository,
+    IUnitOfWork unitOfWork
     ) : IAuthenticationService
 {
     // Hash pré-computado usado para manter tempo de resposta constante
@@ -26,8 +28,8 @@ public class AuthenticationService(
     {
         if (!Email.IsValid(loginRequest.Email))
         {
-            //adicionar Log no futuro
             passwordHasher.VerifyPassword(loginRequest.Password, DummyHash);
+            await LogAsync(EventType.LoginFailed, ipAddress, userAgent);
             return null;
         }
         var email = new Email(loginRequest.Email);
@@ -36,22 +38,41 @@ public class AuthenticationService(
         if (user is null)
         {
             passwordHasher.VerifyPassword(loginRequest.Password, DummyHash);
-            //adicionar log no futuro
+            await LogAsync(EventType.LoginFailed, ipAddress, userAgent);
             return null;
         }
 
         if (!user.IsActive)
         {
-            //adicionar log no futuro
+            await LogAsync(EventType.LoginFailed, ipAddress, userAgent, user);
             return null;
         }
 
         if (!passwordHasher.VerifyPassword(loginRequest.Password, user.PasswordHash))
         {
-            //adicionar log no futuro
+            await LogAsync(EventType.LoginFailed, ipAddress, userAgent, user);
             return null;
         }
 
+        await LogAsync(EventType.LoginSuccess, ipAddress, userAgent, user);
         return new LoginResponse(user.Guid.ToString(), user.Email.Value, user.Name);
+    }
+
+    private async Task LogAsync(
+        EventType eventType,
+        string? ipAddress,
+        string? userAgent,
+        User? user = null)
+    {
+        var log = new AuditLog
+        {
+            EventType = eventType,
+            IpAddress = ipAddress,
+            UserAgent = userAgent,
+            User = user
+        };
+
+        auditLogRepository.Add(log);
+        await unitOfWork.SaveChangesAsync();
     }
 }
