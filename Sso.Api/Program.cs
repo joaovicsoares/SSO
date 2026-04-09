743bf8fb0ba4abeec174a105bf7b0a0dad3c1214
joaovicsoares <victorjoaosoa@gmail.com>
2026-04-09 20:40:30 -0300

    default endpoint removed

--- Sso.Api/Program.cs
+++ Sso.Api/Program.cs
@@ -150,25 +150,6 @@ app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapHealthChecks("/health");
 app.MapControllers();
 
@@ -180,8 +161,3 @@ if (!app.Environment.IsEnvironment("Testing"))
 app.Run();
 
 public partial class Program { }
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
